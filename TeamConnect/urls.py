c27dee37e7244544ca587ebfca6658bc273e0b6d
swethadomatoti <swethadomatoti@gmil.com>
2026-01-09 16:57:36 +0530

    Redirect root URL to chat app

--- TeamConnect/urls.py
+++ TeamConnect/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('chat.urls')),
+    path('', RedirectView.as_view(url='/chat/', permanent=False)),
    # path('api/', include('chat.urls')),
 ]
