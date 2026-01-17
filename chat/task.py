ad0ad146771d89b230e3ced58f6c11455f0b2393
swethadomatoti <swethadomatoti@gmil.com>
2026-01-17 16:46:13 +0530

    Skip email sending on Render to avoid network unreachable error

--- chat/task.py
+++ chat/task.py
@@ -1,16 +1,36 @@
 from celery import shared_task
 from django.core.mail import send_mail
 from django.conf import settings
+
+from django.core.mail import send_mail
+from django.conf import settings
+import os
+
+def send_welcome_celeryemail(username, user_email):
+    subject = "Welcome!"
+    message = f"Hi {username}, welcome to TeamConnect!"
+
+    # Check if running on Render (no email sending allowed)
+    if os.environ.get('RENDER'):
+        print(f"Skipping email send to {user_email} (Render does not allow SMTP in free plan).")
+        return
+
+    try:
+        send_mail(subject, message, settings.EMAIL_HOST_USER, [user_email], fail_silently=False)
+        print(f"Email sent successfully to {user_email}")
+    except Exception as e:
+        print(f"Email sending failed: {e}")
+
  
 
-@shared_task # Marks the function as a Celery task
-def send_welcome_celeryemail(username,user_email):
-    subject = "Welcome to Django App!"
-    message = f"Hi {username},\n\nThank you for registering. You can now log in using your credentials."
-    send_mail(subject, message, settings.EMAIL_HOST_USER, [user_email], fail_silently=False)
-    print(f"Welcome email sent to {user_email}") # debug output
-    return "Email Sent Successfully" # Return a success message
-@shared_task
+# @shared_task # Marks the function as a Celery task
+# def send_welcome_celeryemail(username,user_email):
+#     subject = "Welcome to Django App!"
+#     message = f"Hi {username},\n\nThank you for registering. You can now log in using your credentials."
+#     send_mail(subject, message, settings.EMAIL_HOST_USER, [user_email], fail_silently=False)
+#     print(f"Welcome email sent to {user_email}") # debug output
+#     return "Email Sent Successfully" # Return a success message
+# @shared_task
 def send_otp_email(user, otp):
     subject = 'Your Password Reset OTP'
     message = f"""Hi {user.username},
